4c5f067e27d51584417df20a081a78ed992f2014
Paul Friedman <p-friedman@northwestern.edu>
2010-12-07 14:30:09 -0600

    completion? of monty hall problem

--- monty_hall/game.rb
+++ monty_hall/game.rb
@@ -1,18 +1,30 @@
 class Game
   
-  def initialize(args = nil)
-    car_door_idx = rand(3)
+  attr_accessor :car_door_number
+  attr_accessor :chosen_door_number
+  
+  def initialize(car_door_number = nil)
+    @car_door_number = car_door_number || rand(3)
     @doors = []
     3.times { |x| @doors << Door.new("goat") }
-    @doors[car_door_idx].prize = "car"
+    @doors[@car_door_number].prize = "car"
   end
   
   def doors
     @doors
   end
   
-  def chosen_door_number
-    rand(3)
+  def choose_door(chosen_door_number)
+    @chosen_door_number = chosen_door_number
+    self
+  end
+  
+  def opened_door_number
+    ([0,1,2] - [chosen_door_number, car_door_number]).first
+  end
+  
+  def stay_winner?
+    chosen_door_number == car_door_number
   end
   
 end
@@ -23,6 +35,39 @@ class Door
   
   def initialize(prize)
     @prize = prize
+  end
+  
+end
+
+class Simulation
+  
+  attr_accessor :swap_win_count
+  attr_accessor :stay_win_count
+  
+  def run(number_of_games)
+    @stay_win_count = 0
+    @swap_win_count = 0
+    number_of_games.times do
+      game = Game.new
+      game.choose_door(rand(3))
+      if game.stay_winner?
+        @stay_win_count += 1 
+      else
+        @swap_win_count += 1
+      end
+    end
+  end
+  
+  def swap_win_percentage
+    (@swap_win_count.to_f / total) * 100
+  end
+  
+  def stay_win_percentage
+    100 - swap_win_percentage
+  end
+  
+  def total
+    @stay_win_count + @swap_win_count
   end
   
 end
--- monty_hall/game_spec.rb
+++ monty_hall/game_spec.rb
@@ -27,26 +27,61 @@ describe Game do
     goat_doors.size.should == 2
   end
   
-  it "should not always have a car behind door #3" do
+  it "should not always have a car behind the same door" do
     car_doors = []
-    10.times do |x|
+    20.times do |x|
       game = Game.new
       game.doors.each_with_index {|door, index| car_doors << index if door.prize == "car"}
     end
     car_doors.uniq.size.should > 1
+    car_doors.uniq.size.should == 3
   end
   
-  it "should choose a user selected door at random" do
-    chosen_doors = []
-    10.times do
-      game = Game.new
-      chosen_doors << game.chosen_door_number
-    end
-    chosen_doors.uniq.size.should > 1
+  it "should allow player to choose a door" do
+    game.choose_door(2).chosen_door_number.should == 2
   end
   
-  it "should know if the chosen door won the car" do
+  it "should know if the chosen door is a winner" do
+    game.choose_door(game.car_door_number).should be_stay_winner
+  end
+  
+  it "should allow the ability to set the car door 0" do
+    game = Game.new(0)
+    game.car_door_number.should == 0
+    game.doors.collect(&:prize).should == %w(car goat goat)
+  end
 
+  it "should allow the ability to set the car door 1" do
+    game = Game.new(1)
+    game.doors.collect(&:prize).should == %w(goat car goat)
+  end
+  
+  it "should allow the ability to set the car door 2" do
+    game = Game.new(2)
+    game.doors.collect(&:prize).should == %w(goat goat car)
+  end
+  
+  it "should pick a goat door to show the player if the player chose the car door" do
+    Game.new(1).choose_door(1).opened_door_number.should == 0
+  end
+  
+  it "should pick a goat door to show the player if the player chose the goat door 1" do
+    Game.new(0).choose_door(1).opened_door_number.should == 2
+  end
+  
+  it "should pick a goat door to show the player if the player chose the goat door 2" do
+    Game.new(0).choose_door(2).opened_door_number.should == 1
+  end
+  
+  describe "simulation" do
+    let(:simulation) { Simulation.new }
+    
+    it "should win with a swap strategy 66% of the time and with a stay strategy 33% of the time" do
+      simulation.run(50000)
+      (65...67).should include(simulation.swap_win_percentage)
+      (32...34).should include(simulation.stay_win_percentage)
+    end
+    
   end
   
 end
